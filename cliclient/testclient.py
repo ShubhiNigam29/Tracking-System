cbd47e4a2bccaed05492ac2b3b934d8e1591378f
SHUBHI NIGAM <54998601+ShubhiNigam29@users.noreply.github.com>
2021-01-11 03:09:55 -0500

    Test CLI Client

---
+++ cliclient/testclient.py
@@ -0,0 +1,47 @@
+from pytest import fixture
+from cliclient import init_service
+
+@fixture
+def socket():
+    class Socket(object):
+        def recv_string(self):
+            return 'trackingservice 101 0123456789 pytest data string'
+    return Socket()
+
+
+def test_client_output_1(capfd, socket):
+    """ 
+        Case 1: No account id filter is provided
+    """
+    init_service(socket, aggregation_batch=5, msg_limit=10)
+    out, err = capfd.readouterr()
+
+    outcome = ['pytest data string']*10
+    outcome.extend([
+        'Aggregation for accountId 101: 10 messages received.',
+        'Initiate service shutdown, limit of 10 messages reached.'
+    ])
+    assert out == '\n'.join(outcome) + '\n'
+
+def test_client_output_2(capfd, socket):
+    """ 
+        Case 2: Neither account id filter not aggregation batch is provided
+    """
+    init_service(socket, msg_limit=5)
+    out, err = capfd.readouterr()
+
+    outcome = ['pytest data string']*5
+    outcome.extend(['Initiate service shutdown, limit of 5 messages reached.'])
+    assert out == '\n'.join(outcome) + '\n'
+
+def test_client_output_3(capfd, socket):
+    """ 
+        Case 3: Filter on account id is provided, but no aggregation batch
+    """
+    init_service(socket, msg_limit=5, filter_account_id=105)
+    out, err = capfd.readouterr()
+
+    outcome = [
+        'Initiate service shutdown, limit of 5 messages reached.'
+    ]
+    assert out == '\n'.join(outcome) + '\n'
